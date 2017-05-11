322f797ed60a618868b400c151286d65337a038c
tweety <lzhts@163.com>
2017-05-12 02:20:30 +0800

    Control the Car within console

--- Car.py
+++ Car.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import sys
-import time
+import time,random
+
 FWD= 14
 BAK= 15
 LEFT = 17
@@ -10,13 +11,14 @@ KEEP = 10000
 STOP = 10001
 MIDDLE = 10002
 
-STEPSIZE = 0.01
+STEPSIZE = 0.001
 
 class Controller:
     def __init__(self):
         self.initializeGPIO()
     
     def initializeGPIO(self):
+        print("Controller initiating...")
         GPIO.setwarnings(True)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(FWD,GPIO.OUT,initial=GPIO.HIGH)
@@ -29,7 +31,6 @@ class Controller:
     def left(self):
         if self.STATE_X == RIGHT:
             self.middle()
-            self.keep(1)
         print("left")
         GPIO.output(LEFT,GPIO.LOW)
         self.STATE_X = LEFT
@@ -37,7 +38,6 @@ class Controller:
     def right(self):
         if self.STATE_X == LEFT:
             self.middle()
-            self.keep(1)
         print("right")
         GPIO.output(RIGHT,GPIO.LOW)
         self.STATE_X = RIGHT
@@ -45,16 +45,16 @@ class Controller:
     def fwd(self):
         if self.STATE_Y == BAK:
             self.stop()
-            self.keep(1)
         print("forward")
         GPIO.output(FWD,GPIO.LOW)
+        self.STATE_Y = FWD
 
     def bak(self):
         if self.STATE_Y == FWD:
             self.stop()
-            self.keep(1)
         print("back")
         GPIO.output(BAK,GPIO.LOW)
+        self.STATE_Y = BAK
 
     def stop(self):
         print("stop")
@@ -68,22 +68,10 @@ class Controller:
         GPIO.output(RIGHT,GPIO.HIGH)
         self.STATE_X = MIDDLE
 
-    def keep(self,step=0):
+    def keep(self,steps=0):
         print("keep",steps)
         time.sleep(STEPSIZE*steps)
-
-    def checkStatus(self):
-        print("Checking... Please you should now check your Autobot status to see if that is right.")
-        fwd()
-        bak()
-        stop()
-        left()
-        left()
-        right()
-        right()
-        middle()
-        print("All Check Done!")
-
+    
     def handleMove(self, movement):
         if movement==FWD:
             self.fwd()
@@ -98,18 +86,33 @@ class Controller:
         elif movement == MIDDLE:
             self.middle()
         elif movement == KEEP:
-            self.keep(1)
-    
-    def runconsole(self):
-        keymap = {'w': FWD, 's':BAK, 'a':LEFT, 'd':right}
-        while True:
-            mv = raw_input('')
-            self.handleMove(keymap[mv])
+            self.keep(100)
 
-def main():
-    cc = Controller()
-    cc.runconsole()
-    GPIO.cleanup()
-    
-if __name__ =='__main__':
-    main()
+class EventEmitter:
+    def __init__(self):
+        pass
+
+class Car:
+    def __init__(self):
+        self.ctrl = Controller()
+        self.operations=[FWD,BAK,LEFT,RIGHT,KEEP,STOP,MIDDLE]
+
+    def get_Controller():
+        return self.ctrl
+
+    def operation_emit_random(self):
+        event = random.randint(0,len(self.events)-1)
+        return self.events[event]
+
+    def start(self):
+        try:
+            while True:
+                op = self.operation_emmit_random()
+                self.ctrl.handleMove(op)
+                self.ctrl.handleMove(KEEP)
+        except  Exception as ex:
+            print ex
+        finally:
+            self.ctrl.handleMove(STOP)
+            self.ctrl.handleMove(MIDDLE)
+            GPIO.cleanup()
--- checkGPIO.py
+++ checkGPIO.py
@@ -2,12 +2,65 @@
 import RPi.GPIO as GPIO
 import sys
 import time
-import MovementController
+import Car
 
-def main():
-    mc = MovementController.Controller()
-    mc.checkStatus()
+def checkController():
+    print("Checking... Please you should now check your Autobot status to see if that is right.")
+    car = Car.Car()
+    controller = car.get_Controller()
+    controller.fwd()
+    controller.keep(200)
+    controller.bak()
+    controller.keep(300)
+    controller.stop()
+    controller.keep(500)
+    controller.left()
+    controller.keep(200)
+    controller.middle()
+    controller.keep(400)
+    controller.left()
+    controller.keep(400)
+    controller.right()
+    controller.keep(200)
+    controller.middle()
+    controller.keep(400)
+    controller.right()
+    controller.keep(400)
+    controller.middle()
     GPIO.cleanup()
+    print("All Check Done!")
 
+def run_console():
+    import curses
+    try:
+        keymap = {ord('w'): Car.FWD, ord('s'):Car.BAK, ord('a'):Car.LEFT, ord('d'):Car.RIGHT}
+        stdscr = curses.initscr()
+        curses.cbreak()
+        curses.noecho()
+        stdscr.keypad(1)
+        stdscr.addstr(0,5,"Hit 'q' to quit")
+        stdscr.refresh()
+        mv = ''
+        car = Car.Car()
+        controller = car.get_Controller()
+        while mv != ord('q'):
+            mv = stdscr.getch()
+            if (mv==ord(' ')):
+                controller.handleMove(Car.STOP)
+                controller.handleMove(Car.MIDDLE)
+            else:
+                evnt = keymap[mv]
+                controller.handleMove(evnt)
+            stdscr.refresh()
+    except Exception as ex:
+        print ex
+    finally:
+        GPIO.cleanup()
+        curses.endwin()
+
+def testCar():
+    mc = Car.Car()
+    mc.start()
+    
 if __name__ =='__main__':
-    main()
+    run_console()
